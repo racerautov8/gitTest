ae880ee5c62b444a05f5e84b1608c516028a31a9
Yeedman <timi.moi@hotmail.fi>
2018-01-15 14:58:18 +0200

    another one

---
+++ yet another one.cmd
@@ -0,0 +1,19 @@
+@echo off
+echo if you're doing pushups this one's for you
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
+echo another one
+pause
